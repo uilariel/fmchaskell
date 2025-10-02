fc4324b93e1b4fcffd1860218e198ab2d3cb4904
William Ariel <arielbestafera@gmail.com>
2025-10-02 13:57:48 -0300

    curry and uncurry

--- solutions/FMCFun.hs
+++ solutions/FMCFun.hs
@@ -12,13 +12,13 @@ import Prelude hiding
 
 -- curry takes a "traditional" binary function
 -- and returns its currified version
-curry :: undefined
-curry = undefined
+curry :: ((a, b) -> c) -> a -> b -> c
+curry f a b = f (a, b)
 
 -- uncurry takes a currified function
 -- and returns its "traditional" binary version
-uncurry :: undefined
-uncurry = undefined
+uncurry :: (a -> b -> c) -> (a, b) -> c
+uncurry f (a, b) = f a b
 
 -- flip takes a (currified) binary function
 -- and returns one that behaves the same but takes its arguments in the opposite order
@@ -26,6 +26,13 @@ uncurry = undefined
 -- (.) takes two composable functions and returns their composition
 
 -- (.>) is composition but in diagramatic notation (should be ; but Haskell forbids)
+flip :: (a -> b -> c) -> b -> a -> c
+flip f b a = f a b
+
+(.) :: (b -> c) -> (a -> b) -> a -> c
+f . g = \x -> f (g x)
+
+(.>) :: (a -> b) -> (b -> c) -> a -> c
 (.>) = flip (.)
 
 -- ($) takes a function and a suitable argument and applies the function to the argument
